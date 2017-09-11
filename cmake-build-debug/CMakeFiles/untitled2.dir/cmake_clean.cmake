32f32eea5c4340dbfad6d605fade86184003d3d9
Nélio <neliocmsampaio@gmail.com>
2017-09-11 18:03:26 -0300

    inicio simulador

---
+++ cmake-build-debug/CMakeFiles/untitled2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled2.dir/main.c.o"
+  "CMakeFiles/untitled2.dir/binary_writer.c.o"
+  "untitled2.pdb"
+  "untitled2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
